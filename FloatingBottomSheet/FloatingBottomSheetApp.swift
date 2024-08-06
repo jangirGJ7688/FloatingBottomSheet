526ca64b5ab17a7dcc95269771c0add54228d101
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-08-06 20:31:45 +0530

    Initial Commit

---
+++ FloatingBottomSheet/FloatingBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingBottomSheetApp.swift
+//  FloatingBottomSheet
+//
+//  Created by Ganpat Jangir on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
